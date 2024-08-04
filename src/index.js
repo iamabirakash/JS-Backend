b48dea12d3edb92c61227c7814a58ac7b8e11bf4
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-08-04 20:30:23 +0530

    7 Connected to Mongo Atlas

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 // require('dotenv').config({path: './env'})
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import { DB_NAME } from './constants';
-import express from 'express'
-import connectDB from './db';
-const app = express()
+import connectDB from './db/index.js';
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+// .then(() => {
+//     app.listen(process.env.PORT || 8000, () => {
+//         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+//     })
+// })
+// .catch((err) => {
+//     console.log("MONGO db connection failed !!! ", err);
+// })
 
 
 /*
