d9af345f55435e90c77d7a56658076c2e6c8ed03
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-08-05 16:00:30 +0530

    14 Added AuthMiddleware js

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,33 @@
+
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken"
+import { User } from "../models/user.model.js";
+
+export const verifyJWT = asyncHandler(async(req, _, next) => {
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+        //.replace("Bearer ", ""): If the "Authorization" header exists and contains a token prefixed with "Bearer ", this method removes the "Bearer " prefix, leaving just the token.
+        // .replace("Bearer ", ""): Replaces the string "Bearer " with an empty string, effectively removing the prefix.
+
+        // console.log(token);
+        if (!token) {
+            throw new ApiError(401, "Unauthorized request")
+        }
+
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+
+        if (!user) {
+
+            throw new ApiError(401, "Invalid Access Token")
+        }
+
+        req.user = user;
+        next()
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid access token")
+    }
+
+})
