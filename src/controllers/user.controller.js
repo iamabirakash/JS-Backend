18e353321eba8c950611903d17391cbe17747cf3
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-08-05 20:52:11 +0530

    17 Added updateAccountDetails in userController

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -276,6 +276,29 @@ const getCurrentUser = asyncHandler(async(req, res) => {
     ))
 })
 
+const updateAccountDetails = asyncHandler(async(req, res) => {
+    const {fullName, email} = req.body
+
+    if (!fullName || !email) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const user = await User.findByIdAndUpdate( //Model.findByIdAndUpdate(id, update, options, callback);
+        req.user?._id,
+        {
+            $set: {
+                fullName,
+                email: email
+            }
+        },
+        {new: true} //this return the updated value
+
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details updated successfully"))
+});
 
 export {
     registerUser,
